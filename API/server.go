ca2c94ce611199a769052d2a5f76f959efdb29c0
Brian Denicola <bjd145@yahoo.com>
2018-08-18 15:38:36 -0500

    Working code

--- API/server.go
+++ API/server.go
@@ -3,40 +3,93 @@ package main
 import (
 	"encoding/json"
 	"math/rand"
-	"net/http"
+	"io/ioutil"
 	"time"
 	"os"
+	"net/http"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
+
+type KeyAmount struct {
+	NumberOfKeys int
+}
 
 type Random struct {
 	Time string
     Host string
-    Number int
+	Number int
+	AesKey string
 }
 
-type RandomNumberAPI struct {
-}
-
-func (p *RandomNumberAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/api/random" {
-		random := getRandom(w, r)
-		json.NewEncoder(w).Encode(random)
-		return
+func getKey() (string) {
+	key := make([]byte, 64) 
+	if _, err := rand.Read(key); err != nil {
+		panic(err.Error())
 	}
-	http.NotFound(w, r)
-	return
+	return(base64.StdEncoding.EncodeToString(key))
 }
 
-func getRandom(w http.ResponseWriter, r *http.Request) (Random) {
+func createMessage() (Random) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	max := 1000
 	min := 0
 	host, _ := os.Hostname()
-	msg := Random{ time.Now().Format(time.RFC850), host, rand.Intn(max-min) }
+	msg := Random{ 
+		time.Now().Format(time.RFC850), 
+		host, 
+		rand.Intn(max-min),
+		getKey()}
 	return msg
 }
 
+func generateRandomKeys(len int) []Random {
+	keys := []Random{createMessage()}
+
+	i := 0
+	for i < (len-1) {
+		keys = append(keys, createMessage())
+		i += 1
+	}
+	return keys
+}
+
+type newAPIHandler struct { }
+
+func (eh *newAPIHandler) getAesKeysHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	len := 1
+	keys := generateRandomKeys(len)
+	json.NewEncoder(w).Encode(keys)
+}
+
+func (eh *newAPIHandler) newAesKeyHandler(w http.ResponseWriter, r *http.Request) {
+	var k KeyAmount
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &k)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	keys := generateRandomKeys(k.NumberOfKeys)
+	fmt.Println("Number of Keys - ", k.NumberOfKeys)
+	json.NewEncoder(w).Encode(keys)
+}
+
+func (eh *newAPIHandler) optionsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+
 func main() {
-	mux := &RandomNumberAPI{}
-	http.ListenAndServe(":8000", mux)
+	handler := newAPIHandler{}
+
+	r := mux.NewRouter()
+	apirouter := r.PathPrefix("/api").Subrouter()
+	apirouter.Methods("GET").Path("/keys").HandlerFunc(handler.getAesKeysHandler)
+	apirouter.Methods("POST").Path("/keys").HandlerFunc(handler.newAesKeyHandler)
+	apirouter.Methods("OPTIONS").Path("/keys").HandlerFunc(handler.optionsHandler)
+
+	server := cors.Default().Handler(r)
+	log.Fatal(http.ListenAndServe(":8081", server))
 }
---
+++ ui/js/app.js
@@ -0,0 +1,56 @@
+var server_url = "http://localhost:8081";
+
+Vue.component('key-item', {
+  props: ['k', 'index'],
+  template:'\
+  <span>\
+        <h6 class="list-group-item-heading">Host: {{k.Host }}</h6>\
+        <p class="list-group-item-text" v-if="k.Time">Time Generated: {{ k.Time }}</p>\
+        <p class="list-group-item-text" v-if="k.AesKey">Key: {{ k.AesKey }}</p>\
+  </span>',
+  methods: {}
+});
+
+var app = new Vue({
+  el: '#keys',
+
+  data: {
+    k: { Time: '', Host: '', Number: '', AesKey: '', NumberOfKeys: '' },
+    keys: []  
+  },
+  delimiters: ['${', '}'],
+  
+  mounted: function () {
+    this.fetchKey();
+  },
+
+  methods: {
+    fetchKey: function () {
+      var keys = [];
+      this.$http.get("http://server_url:8081/api/keys")
+        .then(response => response.json())
+        .then(result => {
+          Vue.set(this.$data, 'keys', result);
+          console.log("success in getting keys")  
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    },
+
+    fetchKeys: function () {
+      var keys = [];
+      if (this.k.NumberOfKeys.trim()) {
+        this.$http.post("http://$server_url/api/keys", { "NumberOfKeys": parseInt(this.k.NumberOfKeys.trim(), 10) } )
+          .then(response => response.json())
+          .then(result => {
+            Vue.set(this.$data, 'keys', result);
+            console.log("success in getting keys")  
+          })
+          .catch(err => {
+            console.log(err);
+         });
+      }
+    }
+  }
+});
