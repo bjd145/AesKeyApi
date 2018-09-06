66e30494792387f87c99bf7f9d4a2515e7d9ec09
Brian Denicola <bjd145@yahoo.com>
2018-09-06 14:53:51 -0500

    Code Fixes for URL in app.js

--- src/ui/js/app.js
+++ src/ui/js/app.js
@@ -1,4 +1,4 @@
-const server_url = "http://bjdweb003.azurewebsites.net";
+const server_url = "https://bjdweb003.azurewebsites.net";
 
 Vue.component('key-item', {
   props: ['k', 'index'],
@@ -27,7 +27,7 @@ var app = new Vue({
   methods: {
     fetchKey: function () {
       var keys = [];
-      this.$http.get("http://server_url/api/keys")
+      this.$http.get(server_url + "/api/keys")
         .then(response => response.json())
         .then(result => {
           Vue.set(this.$data, 'keys', result);
@@ -41,7 +41,7 @@ var app = new Vue({
     fetchKeys: function () {
       var keys = [];
       if (this.k.NumberOfKeys.trim()) {
-        this.$http.post("http://$server_url/api/keys", { "NumberOfKeys": parseInt(this.k.NumberOfKeys.trim(), 10) } )
+        this.$http.post(server_url + "/api/keys", { "NumberOfKeys": parseInt(this.k.NumberOfKeys.trim(), 10) } )
           .then(response => response.json())
           .then(result => {
             Vue.set(this.$data, 'keys', result);
